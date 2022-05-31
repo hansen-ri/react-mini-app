4ee82cd693245a9ac18957353820e183c3056f92
hansen-ri <gridironhero@gmail.com>
2022-05-31 13:32:47 -0600

    Modal Implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My To-Dos</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Get a React Job'/>
+      <Todo text='Get paid the big bucks'/>
+      <Modal/>
+      <Backdrop/>
+    </div>);
 }
 
 export default App;
