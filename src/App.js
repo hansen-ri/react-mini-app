94227aaf9a84973b336e8fcf4992fd0e42f0d505
hansen-ri <gridironhero@gmail.com>
2022-06-01 10:51:39 -0600

    Styling with CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import Backdrop from "./components/Backdrop";
-import Modal from "./components/Modal";
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupsPage from "./pages/NewMeetup";
 
 function App() {
   return (
     <div>
-      <h1>My To-Dos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Get a React Job'/>
-      <Todo text='Get paid the big bucks'/>
-      <Modal/>
-      <Backdrop/>
-    </div>);
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,32 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-import Modal from './Modal';
-import Backdrop from './Backdrop';
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const [ modalIsOpen, setModalIsOpen ] = useState(false);
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
 
-    function deleteHandler() {
-        setModalIsOpen(true);
-    }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
 
-    return (
-        <div className="card">
-        <h2>{props.text}</h2>
-        <div className="actions">
-          <button className="btn" onClick={deleteHandler}>Delete</button>
-        </div>
-        {/* If 'modalIsOpen' is true, then '<Modal/>', else 'null' */}
-        { modalIsOpen && <Modal/> } {/* {modalIsOpen ? <Modal/> : null} */}
-        { modalIsOpen && <Backdrop/> }
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
-    );
+      {modalIsOpen && 
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
+    </div>
+  );
 }
 
 export default Todo;
